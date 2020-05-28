ab36d625a9067f9e2e688cc253a80264a041e932
Sudarshan Guttula <sidguttula@yahoo.com>
2020-05-28 14:41:02 -0700

    Driver class instantiating Attack monitor

---
+++ JOptionPane IO/MonsterAttackDriver.java
@@ -0,0 +1,10 @@
+package lab4;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+public class MonsterAttackDriver {
+	public static void main(String[] args) {
+		AttackMonitor numberAttack = new AttackMonitor();
+		numberAttack.monitor();
+	}
+}
