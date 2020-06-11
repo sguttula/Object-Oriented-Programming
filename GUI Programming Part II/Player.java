a8c6ae271db6cac7c08b81d793f7ac0491890a77
Sudarshan Guttula <sidguttula@yahoo.com>
2020-06-10 20:55:42 -0700

    Key event handler and adapter

---
+++ GUI Programming Part II/Player.java
@@ -0,0 +1,15 @@
+package application;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javafx.event.EventHandler;
+import javafx.event.Event;
+
+public class Player extends KeyAdapter{
+	boolean upKey = false;
+	boolean downKey = false; 
+	boolean leftKey = false;
+	boolean rightKey = false;
+
+
+}
